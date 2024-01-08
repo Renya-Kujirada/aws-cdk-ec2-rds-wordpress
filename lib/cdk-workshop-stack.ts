bcaff4bb983477070c08dedf02e43561128a80f9
renya <renya@mail.com>
2024-01-08 12:33:53 +0000

    create wordpress app

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,57 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
+
+import { WebServerInstance } from './constructs/web-server-instance';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const vpc = new ec2.Vpc(this, "BlogVpc", {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    const webServer1 = new WebServerInstance(this, 'WebServer1', {
+      vpc,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const webServer2 = new WebServerInstance(this, 'WebServer2', {
+      vpc,
+    });
+
+    const dbServer = new rds.DatabaseInstance(this, "WordPressDB", {
+      vpc,
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_31 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+      databaseName: "wordpress",
+      // multiAZ を true へ
+      multiAz: true,
+    });
+
+    dbServer.connections.allowDefaultPortFrom(webServer1.instance);
+    dbServer.connections.allowDefaultPortFrom(webServer2.instance);
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, "LoadBalancer", {
+      vpc,
+      internetFacing: true,
+    });
+    const listener = alb.addListener("Listener", {
+      port: 80,
+    });
+    listener.addTargets("ApplicationFleet", {
+      port: 80,
+      targets: [new targets.InstanceTarget(webServer1.instance, 80),
+        new targets.InstanceTarget(webServer2.instance, 80)],
+      healthCheck: {
+        path: "/wp-includes/images/blank.gif",
+      },
+    });
+
+    webServer1.instance.connections.allowFrom(alb, ec2.Port.tcp(80));
+    webServer2.instance.connections.allowFrom(alb, ec2.Port.tcp(80));
   }
 }
